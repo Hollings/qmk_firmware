92cb9b10da4307d70bf66d82fadd54e4d4b0582b
Hollings <hollingsfsdf@gmail.com>
2017-07-21 20:19:30 -0700

    Updated keymap with idle LED fade out and fade in

--- keyboards/planck/keymaps/john/keymap.c
+++ keyboards/planck/keymaps/john/keymap.c
@@ -4,10 +4,18 @@
 #include "planck.h"
 #include "action_layer.h"
 #ifdef AUDIO_ENABLE
-  #include "audio.h"
+#include "audio.h"
 #endif
 #include "eeconfig.h"
+#define BACKLIGHT_BREATHING
+#include "timer.h"
 
+// Backlight timeout feature
+#define BACKLIGHT_TIMEOUT 5  // in seconds
+static uint16_t idle_timer = 0;
+static uint8_t second_counter = 0;
+static uint8_t old_backlight_level = -1;
+static bool led_on = true;
 extern keymap_config_t keymap_config;
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
@@ -54,10 +62,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
-  {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-  {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
-  {BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
+  {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
+  {KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
+  {KC_LCTL, KC_LGUI, KC_LALT,   _______  ,   LOWER,  KC_SPC,  KC_SPC,   RAISE,    KC_LCBR, KC_RCBR  ,   _______   , KC_RSPC}
 },
 
 /* Colemak
@@ -108,10 +116,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = {
-  {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC},
-  {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE},
-  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY}
+  {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_DEL},
+  {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, _______  ,   _______   ,       KC_UNDS  ,   KC_PLUS   , KC_PIPE},
+  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME,KC_UP ,  KC_END},
+  {_______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,  KC_LEFT  , KC_DOWN,KC_RGHT }
 },
 
 /* Raise
@@ -126,7 +134,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = {
-  {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC},
+  {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL},
   {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
   {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
@@ -153,7 +161,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
- * |      | Reset|      |      |      |      |      |      |      |      |      |  Del |
+ * |      | Reset|      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|Colemk|Dvorak|Plover|      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -163,18 +171,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = {
-  {_______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL },
-  {_______, _______, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  PLOVER,  _______},
-  {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
+  {_______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, M(0)},
+  {BL_INC, _______, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  PLOVER,   M(1)},
+  {BL_DEC, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______,  M(2)},
+  {BL_TOGG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 }
 
 
 };
 
+
 #ifdef AUDIO_ENABLE
 
-float tone_startup[][2]    = SONG(STARTUP_SOUND);
+/* float tone_startup[][2]    = SONG(STARTUP_SOUND); */
 float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
 float tone_dvorak[][2]     = SONG(DVORAK_SOUND);
 float tone_colemak[][2]    = SONG(COLEMAK_SOUND);
@@ -190,8 +199,44 @@ void persistent_default_layer_set(uint16_t default_layer) {
   eeconfig_update_default_layer(default_layer);
   default_layer_set(default_layer);
 }
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+    if (record->event.pressed) {
+        breathing_speed_set(2);
+        breathing_pulse();
+        switch(id) {
+            case 0:
 
+                SEND_STRING("bradrgs170");
+                return false;
+            case 1:
+
+                SEND_STRING("cd /var/www/");
+                return false;
+            case 2:
+
+                SEND_STRING("vim /etc/nginx/sites-available/");
+                return false;
+        }
+    }
+    return MACRO_NONE;
+}
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+
+  // turn on backlight after it turned off
+  if (record->event.pressed) {
+     #ifdef BACKLIGHT_ENABLE
+         if (led_on == false || old_backlight_level == -1) {
+             if (old_backlight_level == -1) old_backlight_level = get_backlight_level();
+             breathing_speed_set(2);
+             fade_in();
+             led_on = true;
+         }
+     #endif
+     idle_timer = timer_read();
+     second_counter = 0;
+  }
+
   switch (keycode) {
     case QWERTY:
       if (record->event.pressed) {
@@ -220,16 +265,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+
+
+
     case LOWER:
       if (record->event.pressed) {
         layer_on(_LOWER);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
+
       } else {
         layer_off(_LOWER);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       return false;
       break;
+
     case RAISE:
       if (record->event.pressed) {
         layer_on(_RAISE);
@@ -240,6 +290,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+
+
     case BACKLIT:
       if (record->event.pressed) {
         register_code(KC_RSFT);
@@ -289,17 +341,37 @@ void matrix_init_user(void) {
     #endif
 }
 
+void matrix_scan_user(void) {
+    // idle_timer needs to be set one time
+        if (idle_timer == 0) idle_timer = timer_read();
+
+        #ifdef BACKLIGHT_ENABLE
+            if ( led_on && timer_elapsed(idle_timer) > 1000) {
+                second_counter++;
+                idle_timer = timer_read();
+            }
+
+            if ( led_on && second_counter >= BACKLIGHT_TIMEOUT) {
+                old_backlight_level = get_backlight_level();
+                breathing_speed_set(2);
+                fade_out();
+                led_on = false;
+                second_counter = 0;
+            }
+        #endif
+}
+
 #ifdef AUDIO_ENABLE
 
 void startup_user()
 {
     _delay_ms(20); // gets rid of tick
-    PLAY_NOTE_ARRAY(tone_startup, false, 0);
+    // PLAY_NOTE_ARRAY(tone_startup, false, 0);
 }
 
 void shutdown_user()
 {
-    PLAY_NOTE_ARRAY(tone_goodbye, false, 0);
+    // PLAY_NOTE_ARRAY(tone_goodbye, false, 0);
     _delay_ms(150);
     stop_all_notes();
 }
@@ -311,7 +383,10 @@ void music_on_user(void)
 
 void music_scale_user(void)
 {
-    PLAY_NOTE_ARRAY(music_scale, false, 0);
+    // PLAY_NOTE_ARRAY(music_scale, false, 0);
+
+
+
 }
 
 #endif
